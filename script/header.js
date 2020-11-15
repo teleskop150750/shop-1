e5b00bb6f6c9901510e79e0a4e0fed97e32e8999
teleskop150750 <teleskop150750@gmail.com>
2020-11-15 15:59:07 +0300

    ls

--- script/header.js
+++ script/header.js
@@ -1,11 +1,7 @@
 document.querySelector('.nav__link--w').addEventListener('click', (e) => {
-  e.preventDefault();
   localStorage.setItem('catalog', 'woman');
-  window.location.href = 'catalog.html';
 });
 
 document.querySelector('.nav__link--m').addEventListener('click', (e) => {
-  e.preventDefault();
   localStorage.setItem('catalog', 'man');
-  window.location.href = 'catalog.html';
 });
